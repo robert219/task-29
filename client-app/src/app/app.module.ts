4b3e109bb368844ccecb7a38de19fd8eba905d32
Robert Aradei <aradeirobert@gmail.com>
2022-08-31 09:55:09 +0300

    client-app: added lazyloading modules for the required paths

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      [
+        { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(module => module.DashboardModule) },
+        { path: 'registration', loadChildren: () => import('./registration/registration.module').then(module => module.RegistrationModule) },
+        { path: 'welcome', loadChildren: () => import('./welcome/welcome.module').then(module => module.WelcomeModule) },
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
