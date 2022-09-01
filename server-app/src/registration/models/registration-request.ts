3a9d95f4706232103a51027cb727fbd4fcbb2931
Robert Aradei <aradeirobert@gmail.com>
2022-09-01 16:08:51 +0300

    server-app: added validation

--- server-app/src/registration/models/registration-request.ts
+++ server-app/src/registration/models/registration-request.ts
@@ -1,8 +1,31 @@
+import { MinLength, MaxLength, Matches } from 'class-validator';
+
 export class RegistrationRequest {
+  @MaxLength(63)
+  @Matches(/^[a-zA-Z0-9]*$/)
   first_name: string;
+
+  @MaxLength(63)
+  @Matches(/^[a-zA-Z0-9]*$/)
   middle_name?: string;
+
+  @MaxLength(63)
+  @Matches(/^[a-zA-Z0-9]*$/)
   last_name: string;
+
+  @MaxLength(46)
+  @Matches(/^[a-z0-9]+@[a-z0-9]+\\.[a-z]{2,}$/)
   email: string;
-  mobile_number: string;
+
+  @MinLength(4)
+  @MaxLength(10)
+  @Matches(/^[0-9]+$/)
+  phone_number: string;
+
+  @MinLength(8)
+  @MaxLength(15)
+  @Matches(/^.*[0-9].*/)
+  @Matches(/^.*[a-z].*$/)
+  @Matches(/^.*[A-Z].*$/)
   password: string;
 }
